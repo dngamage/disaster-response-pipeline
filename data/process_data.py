285a30bb6298d4a1a9baca89c7740cf697553d17
Dharshana Gamage <36755947+dngamage@users.noreply.github.com>
2021-05-07 13:31:17 +0530

    Add files via upload
    Fixed review comment

--- data/process_data.py
+++ data/process_data.py
@@ -49,21 +49,41 @@ def clean_data(df):
         df -> Combined data containing messages and categories with categories cleaned up
     """
 
-    # Split the categories
-    categories = df['categories'].str.split(pat=';', expand=True)
+    categories = df.categories.str.split(';', expand=True)
 
-    # Fix the categories columns name
+    # select the first row of the categories dataframe
     row = categories.iloc[[1]]
-    category_columns = [category_name.split('-')[0] for category_name in row.values[0]]
-    categories.columns = category_columns
 
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything
+    # up to the second to last character of each string with slicing
+    category_colnames = row.applymap(lambda x: x[:-2]).iloc[0, :].tolist()
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    # Convert category values to just numbers 0 or 1
     for column in categories:
-        categories[column] = categories[column].str[-1]
-        categories[column] = categories[column].astype(np.int)
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1:]
 
-    df = df.drop('categories', axis=1)
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # Based on the figure 8 documentation original mapping is the following: 1 - yes, 2 - no, so I will convert all
+    # the 2's to 0's
+    categories['related'] = categories['related'].replace(2, 0)
+
+    # The child alone column has a single label so that won't be helpful to train my model I will drop that column
+    categories.drop("child_alone", axis=1, inplace=True)
+
+    # Put processed columns back into the main df
+    # concatenate the original dataframe with the new `categories` dataframe
+    df.drop("categories", axis=1, inplace=True)
     df = pd.concat([df, categories], axis=1)
-    df = df.drop_duplicates()
+
+    # Remove duplicates
+    df.drop_duplicates(inplace=True)
 
     return df
 
